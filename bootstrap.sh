920ad4cca516e8fb3a1cbd72590e39716301e598
Mons Anderson <mons@cpan.org>
2019-07-07 22:00:18 +0300

    replace sed/rename with perl

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 # Insert your appname here:
 MYAPPNAME="APPNAME"
 
-find APPNAME -type f -exec sed -i "s/APPNAME/$MYAPPNAME/g" {} \;
-find APPNAME/ -depth -name "*APPNAME*" -exec rename -v APPNAME $MYAPPNAME {} \;
+
+find APPNAME -type f -exec perl -i -lpE "s/APPNAME/$MYAPPNAME/g" {} \;
+find APPNAME/* -depth -name "*APPNAME*" -exec perl -E '($s,$re)=@ARGV;$_=$s;if(s{APPNAME(?=[^/]*$)}{$re}s){ rename $s,$_ or die "$s => $_: $!" }' {} $MYAPPNAME \;
 mv APPNAME $MYAPPNAME
